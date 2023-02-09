189c6472d5b1456cc314442863576ebe35d8ff6a
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-09 11:51:44 -0500

    Add box shadow

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,8 +7,8 @@ import Main from "@/components/layouts/Main";
 export default function Login() {
   return (
     <Main>
-      <div className="h-full flex items-center justify-center">
-        <div className="flex flex-col items-center p-8 rounded gap-4 drop-shadow-lg border">
+      <div className="h-full flex items-center justify-center ">
+        <div className="flex flex-col items-center p-8 rounded gap-4 shadow-lg shadow-gray-500 border">
           <h4 className="font-semibold text-xl">Login to Readit</h4>
           <div className="space-y-1 w-full">
             <Input label="Email" />
--- pages/register.tsx
+++ pages/register.tsx
@@ -8,26 +8,24 @@ export default function Register() {
   return (
     <Main>
       <div className="h-full flex items-center justify-center">
-        <div className="flex flex-col items-center p-8 rounded gap-4 drop-shadow-lg border">
-          <div className="flex flex-col items-center p-8 rounded gap-4 drop-shadow-lg">
-            <h4 className="font-semibold text-xl">Sign up for Readit</h4>
-            <div className="space-y-1 w-full">
-              <Input label="Email" />
-              <Input label="Password" />
-            </div>
+        <div className="flex flex-col items-center p-8 rounded gap-4 shadow-lg shadow-gray-500 border">
+          <h4 className="font-semibold text-xl">Sign up for Readit</h4>
+          <div className="space-y-1 w-full">
+            <Input label="Email" />
+            <Input label="Password" />
+          </div>
 
-            <Button text="Sign up" block />
-            <div className="">
-              <p className="text-sm font-gray-500">
-                Or if you already have an account
-              </p>
-              <Link
-                href="/login"
-                className="underline hover:opacity-70 text-blue-500"
-              >
-                Sign in
-              </Link>
-            </div>
+          <Button text="Sign up" block />
+          <div className="">
+            <p className="text-sm font-gray-500">
+              Or if you already have an account
+            </p>
+            <Link
+              href="/login"
+              className="underline hover:opacity-70 text-blue-500"
+            >
+              Sign in
+            </Link>
           </div>
         </div>
       </div>
