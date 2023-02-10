a09a30bbad29f028286c3637dff65d717e3fa610
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-10 10:35:20 -0500

    Add name and type to Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,12 +1,14 @@
 interface Input {
   label?: string;
+  name: string;
+  type?: string;
 }
 
-export default function Input({ label }: Input) {
+export default function Input({ label, name, type = "text" }: Input) {
   return (
-    <div>
+    <div className="text-black">
       <p className="text-gray-500 text-sm">{label}</p>
-      <input className="border rounded w-full" />
+      <input className="border rounded w-full p-1" name={name} type={type} />
     </div>
   );
 }
