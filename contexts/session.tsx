e88486dca3c6f748c2a6127022d752ba12e620cc
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-13 14:49:44 -0500

    Check for session and set user if available

--- contexts/session.tsx
+++ contexts/session.tsx
@@ -1,4 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+
+import { useFirebase } from "contexts/firebase";
 
 const SessionContext = createContext({ user: null, setUser: () => {} });
 
@@ -8,13 +11,32 @@ const SessionContext = createContext({ user: null, setUser: () => {} });
 
 // register
 
-// user
-
 export const SessionProvider = ({ children }) => {
   //const data = useSessionProvider();
 
   const [user, setUser] = useState(null);
   const value = { user, setUser };
+
+  const { db } = useFirebase();
+
+  // Check if user is logged in
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (sessionStorage.length > 0) {
+        // Get stored name
+        const name = JSON.parse(
+          sessionStorage[Object.keys(sessionStorage)[0]]
+        ).displayName;
+
+        // Get/set user info from firestore
+        getDoc(doc(db, "users", name)).then((info) => {
+          console.log("info", info);
+          console.log("info.data", info.data());
+          setUser(info.data());
+        });
+      }
+    }
+  }, []);
 
   return (
     <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
