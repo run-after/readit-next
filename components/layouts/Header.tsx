5863ccf68c75a881ae71197eef03105e910225e6
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-13 11:27:22 -0500

    Change header button based on user

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
+import { useSession } from "@/contexts/session";
+
 import Image from "next/image";
 import Button from "../Button";
 
 export default function Header() {
+  const { user } = useSession();
+
   return (
     <div className="w-full h-14 bg-black text-white flex items-center justify-between px-4 fixed top-0 border-b border-gray-800">
       <Link href="/" className="flex gap-1 items-center">
@@ -11,7 +15,11 @@ export default function Header() {
         <h6 className="font-bold text-xl text-green-400">Readit</h6>
       </Link>
       <div>
-        <Button text="Sign in" href="/login" />
+        {user ? (
+          <Button text="Log out" />
+        ) : (
+          <Button text="Sign in" href="/login" />
+        )}
       </div>
     </div>
   );
