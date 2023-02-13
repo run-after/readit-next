7c7e749b917471997184edaa1dff4a6c0ef01bb7
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-13 11:27:08 -0500

    Move init firebase call to firebase.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,16 @@
-import { initializeApp } from "firebase/app";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "contexts/session";
+
+import { FirebaseProvider } from "@/contexts/firebase";
 
 import "@/styles/globals.css";
 
-// Initialize firebase
-const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-});
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <FirebaseProvider>
+      <SessionProvider>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </FirebaseProvider>
+  );
 }
