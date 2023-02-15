56b4ff436c0ca4d43110e894351c645d47e89442
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-15 11:47:17 -0500

    Fix height and expand sidebar

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -3,11 +3,11 @@ import Sidebar from "@/components/layouts/Sidebar";
 
 export default function Main({ children }) {
   return (
-    <div className="bg-black h-screen">
+    <div className="bg-black h-full min-h-screen">
       <Header />
       <Sidebar />
       {/* This needs to be dynamic based on */}
-      <div className="pt-16 ml-28 text-white h-full">{children}</div>
+      <div className="pt-16 ml-32 text-white h-full">{children}</div>
     </div>
   );
 }
