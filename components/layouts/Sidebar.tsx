a7b0e31b445e74c2daa194309e5251347c288bdb
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-15 11:48:32 -0500

    Expand width of sidebar and add groups

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
+import { useSession } from "@/contexts/session";
+
 export default function Sidebar() {
+  const { user } = useSession();
+
   return (
-    <div className="w-24 h-screen fixed left-0 top-14 border-r border-gray-700 text-white p-2">
-      Sidebar
+    <div className="w-32 p-4 h-screen fixed left-0 top-14 border-r border-gray-700 text-white space-y-4 min-w">
+      {user?.groups.map((group) => (
+        <div key={group}>
+          <Link href={`/groups/${group}`}>{group}</Link>
+        </div>
+      ))}
     </div>
   );
 }
