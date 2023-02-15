74b6901d0dbe1f2985a63d98ab91bebab0f45b37
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-15 11:46:55 -0500

    Add z-index

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
   };
 
   return (
-    <div className="w-full h-14 bg-black text-white flex items-center justify-between px-4 fixed top-0 border-b border-gray-800">
+    <div className="w-full h-14 bg-black text-white flex items-center justify-between px-4 fixed top-0 border-b border-gray-800 z-10">
       <Link href="/" className="flex gap-1 items-center">
         <Image src="/../public/alien.png" width="40" height="20" alt="alien" />
         <h6 className="font-bold text-xl text-green-400">Readit</h6>
