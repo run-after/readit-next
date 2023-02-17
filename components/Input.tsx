8f197118ebe18b451d34e777662a311a2a954128
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-17 10:39:22 -0500

    Add placeholder and textarea option

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,13 +2,36 @@ interface Input {
   label?: string;
   name: string;
   type?: string;
+  placeholder?: string;
 }
 
-export default function Input({ label, name, type = "text" }: Input) {
+export default function Input({
+  label,
+  name,
+  type = "text",
+  placeholder,
+}: Input) {
+  if (type === "textarea")
+    return (
+      <div className="space-y-1">
+        <p className="text-gray-500 text-sm">{label}</p>
+        <textarea
+          className="border rounded w-full p-2 bg-black"
+          name={name}
+          placeholder={placeholder}
+        />
+      </div>
+    );
+
   return (
-    <div className="text-black">
+    <div className="space-y-1">
       <p className="text-gray-500 text-sm">{label}</p>
-      <input className="border rounded w-full p-1" name={name} type={type} />
+      <input
+        className="border rounded w-full p-2 bg-black"
+        name={name}
+        type={type}
+        placeholder={placeholder}
+      />
     </div>
   );
 }
