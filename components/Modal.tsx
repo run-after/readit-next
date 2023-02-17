d83f47267ee506b935b976f2aa9f36630446a29b
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-17 10:40:00 -0500

    Use children instead of info - change styling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,7 +1,12 @@
-import { useEffect } from "react";
+import { ReactNode, useEffect, MouseEventHandler } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
-export default function Modal({ onClose, info }) {
+interface IModal {
+  onClose: MouseEventHandler<HTMLParagraphElement>;
+  children: ReactNode;
+}
+
+export default function Modal({ onClose, children }: IModal) {
   // Prevent scroll when open
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -10,14 +15,14 @@ export default function Modal({ onClose, info }) {
 
   return (
     <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center backdrop-blur-sm cursor-auto z-20">
-      <div className="w-3/4 h-5/6 bg-white text-black rounded p-4 relative cursor-auto">
+      <div className="w-3/4 h-5/6 bg-black border border-gray-700 rounded relative cursor-auto">
         <p
           className="text-black absolute top-3 right-5 cursor-pointer hover:opacity-50"
           onClick={onClose}
         >
-          <XMarkIcon className="w-6 h-6" />
+          <XMarkIcon className="w-6 h-6 text-white" />
         </p>
-        {info.title}
+        {children}
       </div>
     </div>
   );
