807d90d6118500860b3297876f085353da886c31
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-20 11:56:38 -0500

    Add overflow to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal({ onClose, children }: IModal) {
 
   return (
     <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center backdrop-blur-sm cursor-auto z-20">
-      <div className="w-3/4 h-5/6 bg-black border border-gray-700 rounded relative cursor-auto">
+      <div className="w-3/4 h-5/6 overflow-y-auto bg-black border border-gray-700 rounded relative cursor-auto">
         <p
           className="text-black absolute top-3 right-5 cursor-pointer hover:opacity-50"
           onClick={onClose}
