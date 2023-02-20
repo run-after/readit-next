24d0af68729b99fef8deb1a7cf78f1e3a2699d95
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-20 12:16:19 -0500

    Wire up up/down votes

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -2,9 +2,12 @@ import {
   ArrowDownCircleIcon,
   ArrowUpCircleIcon,
 } from "@heroicons/react/24/solid";
+import { doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
 
 import { IComment } from "@/interfaces";
 import { useSession } from "@/contexts/session";
+import { useFirebase } from "@/contexts/firebase";
 
 import LocalLink from "./LocalLink";
 
@@ -13,17 +16,70 @@ interface CommentProp {
 }
 
 export default function Comment({ comment }: CommentProp) {
-  // Access context
-  const { user } = useSession();
+  // Access contexts
+  const { user, setUser } = useSession();
+  const { db } = useFirebase();
+
+  // Local state
+  const [likeCount, setLikeCount] = useState(comment.likes);
 
   const handleUpVote = async () => {
+    if (!user) return; // TODO: redirect to login
+
+    // Don't allow more than 1 upvote
+    if (user.likes.includes(comment.id)) return;
     try {
-    } catch (e) {}
+      // Update comment with a like
+      await setDoc(doc(db, "comments", comment.id), {
+        ...comment,
+        likes: comment.likes + 1,
+      });
+
+      const tempUser = {
+        ...user,
+        likes: [...user.likes, comment.id],
+        hates: user.hates.filter((hatedComment) => hatedComment !== comment.id),
+      };
+
+      // Update user with a like
+      await setDoc(doc(db, "users", user.displayName), tempUser);
+
+      setUser(tempUser);
+
+      // Update like count
+      setLikeCount(likeCount + 1);
+    } catch (e) {
+      console.log("err", e);
+    }
   };
 
   const handleDownVote = async () => {
+    if (!user) return; // TODO: redirect to login
+
+    // Don't allow more than 1 downvote
+    if (user.hates.includes(comment.id)) return;
     try {
-    } catch (e) {}
+      // Update comment with a like
+      await setDoc(doc(db, "comments", comment.id), {
+        ...comment,
+        likes: comment.likes - 1,
+      });
+
+      const tempUser = {
+        ...user,
+        hates: [...user.hates, comment.id],
+        likes: user.likes.filter((likedComment) => likedComment !== comment.id),
+      };
+
+      // Update user with a like
+      await setDoc(doc(db, "users", user.displayName), tempUser);
+      setUser(tempUser);
+
+      // Update like count
+      setLikeCount(likeCount - 1);
+    } catch (e) {
+      console.log("err", e);
+    }
   };
 
   return (
@@ -46,7 +102,7 @@ export default function Comment({ comment }: CommentProp) {
           />
         </button>
 
-        <span>{comment.likes}</span>
+        <span>{likeCount}</span>
         <button onClick={handleUpVote}>
           <ArrowUpCircleIcon
             className={`h-4 w-4 ${
@@ -59,4 +115,4 @@ export default function Comment({ comment }: CommentProp) {
   );
 }
 
-// TODO: Handle up/down vote
+// TODO: Handle unauthed user
