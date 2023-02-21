9dcba133235cecb93dce1bb9e8b6e97232e8435e
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-21 11:16:19 -0500

    Add heading to Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,9 +4,10 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 interface IModal {
   onClose: MouseEventHandler<HTMLParagraphElement>;
   children: ReactNode;
+  heading?: ReactNode;
 }
 
-export default function Modal({ onClose, children }: IModal) {
+export default function Modal({ onClose, children, heading }: IModal) {
   // Prevent scroll when open
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -17,14 +18,19 @@ export default function Modal({ onClose, children }: IModal) {
 
   return (
     <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center backdrop-blur-sm cursor-auto z-20">
-      <div className="w-3/4 h-5/6 overflow-y-auto bg-black border border-gray-700 rounded relative cursor-auto">
-        <p
-          className="text-black absolute top-3 right-5 cursor-pointer hover:opacity-50"
-          onClick={onClose}
-        >
-          <XMarkIcon className="w-6 h-6 text-white" />
-        </p>
-        {children}
+      <div className="w-3/4 h-5/6 overflow-y-auto bg-black border border-gray-700 rounded relative cursor-auto flex flex-col">
+        {/* Heading */}
+        <div className="w-full h-12 flex gap-4 items-center px-4">
+          <div className="flex-1">{heading}</div>
+          <p
+            className="text-black cursor-pointer hover:opacity-50"
+            onClick={onClose}
+          >
+            <XMarkIcon className="w-6 h-6 text-white" />
+          </p>
+        </div>
+        {/* Main section */}
+        <div className="overflow-y-auto">{children}</div>
       </div>
     </div>
   );
