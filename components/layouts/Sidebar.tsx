926e431debdd0da126a1b6eb162b9709d02a4595
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-02-23 09:15:30 -0500

    Add all groups link

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -3,10 +3,13 @@ import { useSession } from "@/contexts/session";
 import LocalLink from "../LocalLink";
 
 export default function Sidebar() {
+  // Access context
   const { user } = useSession();
 
   return (
     <div className="w-32 p-4 h-screen fixed left-0 top-14 border-r border-gray-700 text-white space-y-4 min-w">
+      <LocalLink href="/groups" text="All groups" />
+
       {user?.groups.map((group) => (
         <div key={group}>
           <LocalLink href={`/groups/${group}`} text={group} />
