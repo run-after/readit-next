33cd9fb8103fcfbf47e19302bada6ee2d3fbb28c
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-07-25 15:20:56 -0400

    Create Error.tsx

---
+++ components/Error.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import Main from "@/components/layouts/Main";
+
+export default function Error() {
+  return (
+    <Main>
+      <div className="flex flex-col items-center gap-4 pt-12">
+        <h1 className="text-3xl font-bold">
+          Oops... This group doesn't exist.
+        </h1>
+        <p>
+          Feel free to explore all the groups in our community{" "}
+          <Link href="/groups" className="underline">
+            here
+          </Link>
+          ...
+        </p>
+      </div>
+    </Main>
+  );
+}
