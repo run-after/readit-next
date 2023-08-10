6dd102250f16c66021541d15ed918d36003d1530
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-08-10 16:27:26 -0400

    Add show more posts button and determine total posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import { getDocs, collection, limit, query, where } from "firebase/firestore";
+import Link from "next/link";
+import {
+  getDocs,
+  collection,
+  limit,
+  query,
+  where,
+  getCountFromServer,
+} from "firebase/firestore";
 
 import { useFirebase } from "@/contexts/firebase";
 import { IPost } from "@/interfaces";
@@ -7,11 +15,14 @@ import { useSession } from "@/contexts/session";
 
 import Main from "@/components/layouts/Main";
 import PostFeed from "@/components/PostFeed";
+import Button from "@/components/Button";
 
 export default function Home() {
   // Local state
   const [allPosts, setAllPosts] = useState<IPost[]>([]);
   const [loading, setLoading] = useState(true);
+  const [numOfPosts, setNumOfPosts] = useState(10);
+  const [displayShowMoreBtn, setDisplayShowMoreBtn] = useState(false);
 
   // Access contexts
   const { db } = useFirebase();
@@ -25,15 +36,20 @@ export default function Home() {
       if (user?.groups.length) {
         theQuery = query(
           collection(db, "posts"),
-          where("group", "in", user.groups),
-          limit(10)
+          where("group", "in", user.groups)
         );
       } else {
-        theQuery = query(collection(db, "posts"), limit(10));
+        theQuery = query(collection(db, "posts"));
       }
 
+      // Get server count
+      const serverCount = await getCountFromServer(theQuery);
+
+      // Determine if to display show more btn
+      setDisplayShowMoreBtn(serverCount.data().count > numOfPosts);
+
       // Get posts
-      const querySnapshot = await getDocs(theQuery);
+      const querySnapshot = await getDocs(query(theQuery, limit(numOfPosts)));
 
       // Add id to each post
       querySnapshot.forEach((doc) => {
@@ -52,7 +68,7 @@ export default function Home() {
 
   useEffect(() => {
     getPosts();
-  }, [user]);
+  }, [user, numOfPosts]);
 
   if (loading)
     return (
@@ -66,9 +82,22 @@ export default function Home() {
   return (
     <Main>
       <PostFeed posts={allPosts} />
+      <div className="flex flex-col md:flex-row gap-4 justify-between px-12 pb-4">
+        {displayShowMoreBtn && (
+          <Button
+            text="Show more posts"
+            size="sm"
+            onClick={() => setNumOfPosts(numOfPosts + 10)}
+          />
+        )}
+        <p className="text-white/70">
+          To see more content, join more{" "}
+          <Link href="/groups" className="underline hover:text-white">
+            groups
+          </Link>
+          ...
+        </p>
+      </div>
     </Main>
   );
 }
-
-// TODO:
-// Pagination
